00db8926dea771fa303e749817a2f975e3b69a00
Roy Mumau <roymumau@gmail.com>
2016-03-08 16:00:15 -0500

    Changed network and security configuration, changed OS from ubuntu to RHEL, added a file to the hard drive

--- example.tf
+++ example.tf
@@ -3,7 +3,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-b8b061d0"
-    instance_type = "t1.micro"
+    ami = "ami-2051294a"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-021ab828"
+    vpc_security_group_ids = ["sg-9d7bc4e5"]
+    associate_public_ip_address = false
+    key_name = "roy-dev"
+
+    user_data = "${file("testfile")}"
 }
 
